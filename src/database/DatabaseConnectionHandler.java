24b0d621c93675e74f1573eb53094a7a347b36d3
yegorye <yegorye@student.ubc.ca>
2022-08-09 14:01:38 -0700

    reverted ORACLE_URL changes. Use depends on connection type

--- src/database/DatabaseConnectionHandler.java
+++ src/database/DatabaseConnectionHandler.java
@@ -15,9 +15,9 @@ import java.util.ArrayList;
  */
 public class DatabaseConnectionHandler {
     // Use this version of the ORACLE_URL if you are running the code off of the server
-    private static final String ORACLE_URL = "jdbc:oracle:thin:@dbhost.students.cs.ubc.ca:1522:stu";
+    // private static final String ORACLE_URL = "jdbc:oracle:thin:@dbhost.students.cs.ubc.ca:1522:stu";
     // Use this version of the ORACLE_URL if you are tunneling into the undergrad servers
-    //public static final String ORACLE_URL = "jdbc:oracle:thin:@localhost:1522:stu";
+    public static final String ORACLE_URL = "jdbc:oracle:thin:@localhost:1522:stu";
     public static final String EXCEPTION_TAG = "[EXCEPTION]";
     public static final String WARNING_TAG = "[WARNING]";
 
